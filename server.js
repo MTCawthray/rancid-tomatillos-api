24bf8c8a0b1f4c04bb669fde3c5f7470507e8405
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-04 12:30:26 -0600

    Create or basic server layout

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const server = express();
+
+server.set('port', process.env.PORT || 3002);
+server.locals.title = 'Rancid Tomatillo Favorites API';
+server.locals.favoritesList = [
+  {
+    "id": 694919,
+    "title": "Money Plane",
+    "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+    "release_date": "2020-09-29",
+    "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+    "genres": [
+    "Action"
+    ],
+    "budget": 0,
+    "revenue": 0,
+    "runtime": 82,
+    "tagline": "",
+    "average_rating": 6.142857142857143
+    }
+]
+
+server.get('/', (request, response) => {
+  response.send('Hello world! This is in server.js')
+});
+
+server.listen(server.get('port'), () => {
+  console.log(`${server.locals.title} is running on http://localhost:${server.get('port')}. `);
+});
+
+server.get('/api/v1/favoritesList', (request, response) => {
+  response.status(200).json(server.locals.favoritesList)
+})
