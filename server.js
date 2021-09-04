ba3a62605e137b89e80d9a52b736fc81b3eec2e1
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-04 13:20:29 -0600

    Add cors dependency and import into server js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 
+server.use(cors());
 server.set('port', process.env.PORT || 3002);
 server.locals.title = 'Rancid Tomatillo Favorites API';
 server.locals.favoritesList = [
