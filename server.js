73e1129963e170722452acacc3a6e09a0c9f77c3
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-05 17:18:30 -0600

    Remove dummy data and return delete request body

--- server.js
+++ server.js
@@ -8,22 +8,7 @@ server.use(cors());
 server.set('port', process.env.PORT || 3002);
 server.locals.title = 'Rancid Tomatillo Favorites API';
 server.locals.favoritesList = [
-  {
-    "id": 694919,
-    "title": "Money Plane",
-    "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-    "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-    "release_date": "2020-09-29",
-    "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
-    "genres": [
-    "Action"
-    ],
-    "budget": 0,
-    "revenue": 0,
-    "runtime": 82,
-    "tagline": "",
-    "average_rating": 6.142857142857143
-    }
+
 ]
 
 server.get('/', (request, response) => {
@@ -39,15 +24,14 @@ server.get('/api/v1/favoritesList', (request, response) => {
 })
 
 server.post('/api/v1/favoritesList', (request, response) => {
+  console.log("requestBody-->", request.body)
    server.locals.favoritesList.push(request.body);
    response.status(201).json(request.body);
 })
 
 server.delete('/api/v1/favoritesList', (request, response) => {
-  console.log('"We made it!", request.body inside delete! --->', request.body)
   const { id } = request.body
-  console.log(id, '<--dataObj')
 
     server.locals.favoritesList = server.locals.favoritesList.filter(favorite => parseInt(favorite.id) !== parseInt(id))
-  response.status(202).send("deleted");
+  response.status(202).send(id);
 })
